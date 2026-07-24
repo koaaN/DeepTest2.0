b2c4759c85ab81c56d4c7bf8e8bafc848a9c3f2f
Jonas Salo <salomannen1234@gmail.com>
2026-07-24 17:23:16 +0200

    Update local changes

--- android-helper/assets/ACE6T/root.bat
+++ android-helper/assets/ACE6T/root.bat
@@ -3,5 +3,5 @@ setlocal
 where adb >nul 2>&1
 adb push preload.so /data/local/tmp/preload.so
 adb shell chmod 0644 /data/local/tmp/preload.so
-adb shell /data/local/tmp/su -c id
+adb shell LD_PRELOAD=/data/local/tmp/preload.so /system/bin/true
 pause
