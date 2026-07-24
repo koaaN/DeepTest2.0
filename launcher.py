ad8508c5a5e04199533417455d6738e33fe9af29
Jonas Salo <salomannen1234@gmail.com>
2026-07-24 17:38:35 +0200

    Route token CLI commands inside packaged app

--- launcher.py
+++ launcher.py
@@ -1,8 +1,11 @@
 import sys
 
-if len(sys.argv) >= 3 and sys.argv[1:3] == ["-m", "deeptesting.cli"]:
-    from deeptesting.cli import main
-    sys.argv = ["deeptesting.cli", *sys.argv[3:]]
+if len(sys.argv) >= 3 and sys.argv[1] == "-m" and sys.argv[2] in {"deeptesting.cli", "deeptesting.token_cli"}:
+    if sys.argv[2] == "deeptesting.token_cli":
+        from deeptesting.token_cli import main
+    else:
+        from deeptesting.cli import main
+    sys.argv = [sys.argv[2], *sys.argv[3:]]
 else:
     from deeptesting.gui import main
 
